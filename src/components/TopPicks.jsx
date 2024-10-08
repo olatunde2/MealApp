a56e524378680baf636d80f84aa22753f0824768
olatunde2 <133256292+olatunde2@users.noreply.github.com>
2024-10-09 00:38:58 +0300

    Update TopPicks.jsx

--- src/components/TopPicks.jsx
+++ src/components/TopPicks.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { topPicks } from "../data/data.js";
+import { topPicks } from '/src/data/data';
+;
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css/core"; // Import only core styles
 
